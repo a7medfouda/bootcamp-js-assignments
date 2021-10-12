c2c414a2f459f07b48a1df45b61481cb1d1a44cb
a7medfouda <84880406+a7medfouda@users.noreply.github.com>
2021-10-12 13:35:15 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+// -----------------------1
+let numberOne = 10;
+let numberTwo = 20;
+
+console.log(numberOne + '' + numberTwo);
+console.log('**************');
+console.log('10' + '20');
+console.log('**************');
+console.log(`${numberOne}` + `${numberTwo}`);
+console.log('**************');
+console.log(numberTwo + '\n' + numberOne);
+console.log('**************');
+console.log(`${numberTwo}
+${numberOne}
+`);
+// -----------------------2
+// create a div in html document and give it an 'id'
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
+// -----------------------3
+console.log(`
+\`I'm In 
+\\\\
+Love \\\\ """ '''
+++ With ++
+\\"""\\"""
+""JavaScript""\`
+`)
+// -----------------------4
+let a = 21;
+let b = 20;
+// _21_2021_2021_2021_20_
+
+console.log('_' + a + '_' + b + '' + a + '_' + `${b}` + `${a}` + '_' + `${b}${''}${a}` + '_' + b + '_');
